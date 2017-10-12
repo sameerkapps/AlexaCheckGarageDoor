22b73b91a361332ebee27840e791d197bc7dc137
sameerkapps <sk_career@hotmail.com>
2017-10-11 22:54:47 -0700

    Added iOS, Android, UWP Xamarin Forms App
    Added iOS, Android, UWP Xamarin Forms App

---
+++ PhoneApp/PhoneApp/PhoneApp/Commands/CheckDoorCommand.cs
@@ -0,0 +1,81 @@
+﻿////////////////////////////////////////////////////////
+// Copyright (c) 2017 Sameer Khandekar                //
+// License: MIT License.                              //
+////////////////////////////////////////////////////////
+using MvvmAtom;
+using PhoneApp.ViewModels;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace PhoneApp.Commands
+{
+    public class CheckDoorCommand : AtomCommandBase
+    {
+        /// <summary>
+        /// Constructor will initialize the client
+        /// </summary>
+        /// <param name="viewModel"></param>
+        public CheckDoorCommand(AtomViewModelBase viewModel)
+            : base(viewModel)
+        {
+            _client = new HttpClient();
+            _client.DefaultRequestHeaders.Add(HeaderKeyName, HeaderKeyVal);
+        }
+
+        /// <summary>
+        /// When device IP gets changed, this will en/disble the command
+        /// </summary>
+        /// <param name="propName"></param>
+        public override void EvaluateCanExecuteChanged(string propName)
+        {
+            if (string.Equals(nameof(MainPageViewModel.DeviceName), propName))
+            {
+                RaiseCanExecuteChanged();
+            }
+        }
+
+        /// <summary>
+        /// Can execute, if there is a string in the Device IP field
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public override bool CanExecute(object parameter)
+        {
+            return !string.IsNullOrWhiteSpace(mainView.DeviceName);
+        }
+
+        /// <summary>
+        /// Obtains status from ESP8266 and updates
+        /// Status and last checked in the view model
+        /// </summary>
+        /// <param name="parameter"></param>
+        public async override void Execute(object parameter)
+        {
+            UriBuilder uriBuilder = new UriBuilder(BaseAddr + mainView.DeviceName);
+
+            try
+            {
+                Debug.WriteLine(uriBuilder.ToString());
+                var responseMessage = await _client.GetAsync(uriBuilder.ToString());
+
+                mainView.Status = await responseMessage.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                mainView.Status = "Error while checking";
+            }
+
+            mainView.LastChecked = DateTime.Now.ToString();
+        }
+
+        HttpClient _client = new HttpClient();
+
+        MainPageViewModel mainView => (MainPageViewModel)base.ViewModel;
+
+        private const string BaseAddr = "https://YOUR SITE.azurewebsites.net/api/GarageDoor?doorName=";
+        private const string HeaderKeyName = "GarageDoorSecurityKey";
+        private const string HeaderKeyVal = "YOUR KEY";
+    }
+}
---
+++ PhoneApp/PhoneApp/PhoneApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,92 @@
+﻿////////////////////////////////////////////////////////
+// Copyright (c) 2017 Sameer Khandekar                //
+// License: MIT License.                              //
+////////////////////////////////////////////////////////
+using System;
+
+using MvvmAtom;
+using PhoneApp.Commands;
+
+namespace PhoneApp.ViewModels
+{
+    /// <summary>
+    /// View model for the web page
+    /// </summary>
+    public class MainPageViewModel : AtomViewModelBase
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainPageViewModel()
+        {
+            CheckDoorCommand = new CheckDoorCommand(this);
+        }
+
+        /// <summary>
+        /// Name of the device
+        /// </summary>
+        private string _deviceName = "GarageDoor1";
+        public string DeviceName
+        {
+            get
+            {
+                return _deviceName;
+            }
+
+            set
+            {
+                if (_deviceName != value)
+                {
+                    _deviceName = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Status returned by the REST API
+        /// </summary>
+        private string _status;
+        public string Status
+        {
+            get
+            {
+                return _status;
+            }
+
+            set
+            {
+                if (_status != value)
+                {
+                    _status = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Last check time
+        /// </summary>
+        private string _lastChked;
+        public string LastChecked
+        {
+            get
+            {
+                return _lastChked;
+            }
+            set
+            {
+                if (_lastChked != value)
+                {
+                    _lastChked = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Command to check door
+        /// </summary>
+        public AtomCommandBase CheckDoorCommand { get; set; }
+    }
+}
